c8e4c26d3b9c1186dfbd49a82101bd5cbceea857
JOAQUIN MIKEL ERMITA <joaquinmikel.ermita@g.msuiit.edu.ph>
2026-06-23 14:40:08 +0800

    feat: add basic page structures for dashboard reports and settings

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,14 @@
+export default function SettingsPage() {
+  return (
+    <div className="w-full my-5 px-4">
+      <div className="mb-6 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+        <div className="ml-3 flex flex-row items-center gap-4">
+          <h1 className="text-2xl font-bold tracking-tight text-gray-700">
+            Settings
+          </h1>
+        </div>
+      </div>
+      Test
+    </div>
+  );
+}
